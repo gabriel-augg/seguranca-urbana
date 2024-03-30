56e597b1ddb804e62cd8de86dd60e19e12806862
Gabriel <gabrielgapseu@gmail.com>
2024-03-30 08:26:41 -0300

    Feat: search page

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,28 @@
+import styles from "./styles.module.css"
+
+export default function Card({id, name, crimeRate, publicLight, recomendation, cep }){
+    return(
+        <div key={id} className={styles.card}>
+            <div>
+                <h1>Bairro</h1>
+                <span>{name}</span>
+            </div>
+            <div>
+                <h1>Taxa de criminalidade</h1>
+                <span>{crimeRate}</span>
+            </div>
+            <div>
+                <h1>Iluminação publica</h1>
+                <span>{publicLight}</span>
+            </div>
+            <div>
+                <h1>Recomendação</h1>
+                <span>{recomendation}</span>
+            </div>
+            <div>
+                <h1>CEP</h1>
+                <span>{cep}</span>
+            </div>
+        </div>
+    )
+}
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,6 +1,8 @@
 import Container from "../../components/Container"
 import Title from "../../components/Title"
+import Card from "../../components/Card"
 
+import ilustration from "../../assets/ilustration.svg"
 import styles from "./styles.module.css"
 
 export default function Search(){
@@ -11,7 +13,7 @@ export default function Search(){
                 <Title title="Procurando um bairro" />
                 <div className={styles.search_area}>
                     <div>
-                        <input type="text" name="search"  />
+                        <input type="number" name="search" placeholder="Digite aqui o cep"  />
                         <button>Buscar</button>
                     </div>
                     <select>
@@ -20,6 +22,13 @@ export default function Search(){
                         <option>Iluminação pública</option>
                         <option>Recomendação de segurança</option>
                     </select>
+                </div>
+                <div className={styles.card_area}>
+                    {/* <Card id="12313" name="Santa maria" crimeRate="5,2" publicLight="Boa" recomendation="Não ande muito tarde a noite" cep="49044274z"  /> */}
+                    <div className={styles.no_card}>
+                        <h1>Não há nenhum card no momento.</h1>
+                        <img src={ilustration} alt="ilustracao" />
+                    </div>
                 </div>
             </section>
         </Container>
